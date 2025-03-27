c064177bd89562732fdb0a176b758b50d6f24bfe
Gloria Gordon-Mensah <82785082+MhizGordon@users.noreply.github.com>
2025-03-27 05:33:57 +0000

    comment history.service.ts

--- Frontend/Expense-Tracker/src/app/services/history.service.ts
+++ Frontend/Expense-Tracker/src/app/services/history.service.ts
@@ -1,19 +1,22 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http'; // Importing HttpClient to make HTTP requests
 import { Injectable } from '@angular/core';
-import { Observable, throwError } from 'rxjs';
+import { Observable, throwError } from 'rxjs'; // Importing Observable to handle async operations and throwError for error handling
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class HistoryService {
-  private apiUrl = 'http://localhost:5000/api/v1';
+  private apiUrl = 'http://localhost:5000/api/v1'; // Base URL for the backend API
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {} // Injecting HttpClient to enable HTTP requests
 
+  // Fetches all income records from the backend
   getIncomes(): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}/get-incomes`);
   }
+
+// Fetches all expense records from the backend
 getExpenses(): Observable<any[]> {
   return this.http.get<any[]>(`${this.apiUrl}/get-expenses`);
 }
