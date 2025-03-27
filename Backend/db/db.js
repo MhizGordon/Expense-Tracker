aaddd69210b4c806acf7a29ed962a7a3f1afac14
Gloria Gordon-Mensah <82785082+MhizGordon@users.noreply.github.com>
2025-03-27 04:32:29 +0000

    comment db.js

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,8 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose');  // Import mongoose for MongoDB connection
 
+// Connects to the MongoDB database
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(process.env.MONGO_URI, {  // Connect to MongoDB using the URI from environment variables
     });
     console.log('MongoDB connected successfully');
   } catch (error) {
