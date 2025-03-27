ddef2841195608ee154d2edbd67fde0811714225
Gloria Gordon-Mensah <82785082+MhizGordon@users.noreply.github.com>
2025-03-27 05:32:20 +0000

    comment income.service.ts

--- Frontend/Expense-Tracker/src/app/services/income.service.ts
+++ Frontend/Expense-Tracker/src/app/services/income.service.ts
@@ -5,15 +5,17 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-export class IncomeService {
-  private apiUrl = 'http://localhost:5000/api/v1';
+export class IncomeService { 
+  private apiUrl = 'http://localhost:5000/api/v1'; // Base URL for the backend API
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }  // Injecting HttpClient to enable making HTTP requests
 
+   // Fetches all income records from the backend
   getIncomes(): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}/get-incomes`);
   }
 
+    // Adds a new income record to the backend
   addIncome(IncomeData: any): Observable<any> {
     return this.http.post<any>(`${this.apiUrl}/add-incomes`,IncomeData);
   }
