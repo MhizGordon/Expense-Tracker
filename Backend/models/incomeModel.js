4bab75006830b403eed4e257d35b4f7a13f999a5
Gloria Gordon-Mensah <82785082+MhizGordon@users.noreply.github.com>
2025-03-27 04:36:12 +0000

    comment incomeModel.js

--- Backend/models/incomeModel.js
+++ Backend/models/incomeModel.js
@@ -1,10 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); // Import mongoose for database modeling
 
+// Define the schema for incomes
 const incomeSchema = new mongoose.Schema({
-  month: { type: String, required: true },
-  date: { type: String, required: true },   
-  source: { type: String, required: true }, 
-  amount: { type: Number, required: true }, 
+  month: { type: String, required: true }, // Month of the income
+  date: { type: String, required: true },   // Date of the income
+  source: { type: String, required: true }, // Category or type of income
+  amount: { type: Number, required: true }, // Income amount
 });
 
+// Export the Income model using the schema
 module.exports = mongoose.model('Income', incomeSchema);
