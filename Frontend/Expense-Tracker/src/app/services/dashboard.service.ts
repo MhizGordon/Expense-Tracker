0eeebb7bda8042ab600ca44b11bbac868dcb5893
Gloria Gordon-Mensah <82785082+MhizGordon@users.noreply.github.com>
2025-03-27 05:26:24 +0000

    comment dashboard.service.ts

--- Frontend/Expense-Tracker/src/app/services/dashboard.service.ts
+++ Frontend/Expense-Tracker/src/app/services/dashboard.service.ts
@@ -3,14 +3,16 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
+// Service to interact with the backend API and fetch dashboard-related data
 @Injectable({
   providedIn: 'root',
 })
 export class DashboardService {
-  private apiUrl = 'http://localhost:5000/api/v1'; // Your API base URL
+  private apiUrl = 'http://localhost:5000/api/v1'; // API base URL for the server
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}  //Inject HttpClient for making HTTP requests
 
+   // Fetches income data for a specified number of months
   getIncomeData(months: number): Observable<{ date: string; month: string; amount: number }[]> {
     return this.http
       .get<{ data: { date: string; month: string; amount: number }[] }>(
@@ -19,15 +21,15 @@ export class DashboardService {
       .pipe(map(response => response.data)); // Extract 'data' from the response
   }
   
-  
 
+  // Fetches expense data for a specified number of months
   getExpenses(months: number): Observable<{ date: string; month: string; expenseAmount: number }[]> {
     return this.http.get<{ data: { date: string; month: string; expenseAmount: number }[] }>(
       `${this.apiUrl}/get-expenses?months=${months}`
     ).pipe(map(response => response.data)); // Extract 'data' from the response
   }
   
-  
+  // Fetches savings data for a specified number of month
   getSavingsData(months: number): Observable<{ month: string; amount: number }[]> {
     return this.http.get<{ month: string; amount: number }[]>(
       `${this.apiUrl}/get-savings?months=${months}`
