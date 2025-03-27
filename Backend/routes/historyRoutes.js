aa8544c00e9f7cbf55351face62fcd2da99bb70c
Gloria Gordon-Mensah <82785082+MhizGordon@users.noreply.github.com>
2025-03-27 04:47:16 +0000

    Comment historyRoutes.js

--- Backend/routes/historyRoutes.js
+++ Backend/routes/historyRoutes.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
+// Import controller functions for handling income, expense, and history
 const { addIncome, getIncomes } = require('../controllers/incomeControllers');
 const { addExpense, getExpenses } = require('../controllers/expenseControllers');
 const {getHistory} = require('../controllers/historyControllers');
 
-
-router.post('/add-income', addIncome)
-      .get('/get-incomes', getIncomes) 
-      .post('/add-expense', addExpense)
-      .get('/get-expenses', getExpenses)
-      .get('/history', getHistory);
+// Define routes for income, expense, and history
+router.post('/add-income', addIncome) // Route to add a new income
+      .get('/get-incomes', getIncomes)  // Route to fetch all incomes
+      .post('/add-expense', addExpense) // Route to add a new expense
+      .get('/get-expenses', getExpenses) // Route to fetch all expenses
+      .get('/history', getHistory); // Route to fetch combined income and expense history
 
 
 module.exports = router;
